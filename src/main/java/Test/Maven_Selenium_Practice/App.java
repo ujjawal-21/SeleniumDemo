ea355589ec1135bf3d70ce33699182f44a99b6a8
ujjawal <ujjawal.anand21@gmail.com>
2022-06-13 16:02:21 +0530

    First round

---
+++ src/main/java/Test/Maven_Selenium_Practice/App.java
@@ -0,0 +1,13 @@
+package Test.Maven_Selenium_Practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Test/Maven_Selenium_Practice/AppTest.java
@@ -0,0 +1,28 @@
+package Test.Maven_Selenium_Practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AppTest {
+
+	WebDriver driver;
+	
+	@BeforeMethod
+	public void init(){
+		System.setProperty("webdriver.chrome.driver", "C:\\Driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void Test1() {
+		WebElement element = driver.findElement(By.name("q"));
+		element.sendKeys("Java");
+	}
+	
+}
